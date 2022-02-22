f00f509a62d4bb6d61b73e3c288007abce572312
zascerta <giammarco.daless@gmail.com>
2022-02-22 18:47:00 +0100

    fixini cut script

--- scripts/data_utils.py
+++ scripts/data_utils.py
@@ -5,18 +5,22 @@ FILE_SCORES = "dataset/WhoScoredFinal1.csv"
 FILE_FBMAN  = "dataset/datafm20.csv"
 
 
-def dataCutSeasonName(season: str, in_file: str=FILE_SCORES, new_file: str="datacut.csv"):
-	new_file = new_file[:-4] + "_PremierLeague_" + season[-4:] + ".csv"
+def dataCutSeasonName(season: str, in_file: str=FILE_SCORES, new_file: str="whoScored.csv"):
+	new_file = new_file[:-4] + season[-4:] + ".csv"
 
 	# read file data
 	df = pd.read_csv(in_file, delimiter=',')
 
-	# query the data
-	cut_df = df.query(f"seasonName == '{season}' & tournamentName == 'Premier League' & teamRegionName == 'England'")
-	cut_df = cut_df.sort_values(by=["teamName"])
+	# query the data by season
+	cut_df = df.query(f"seasonName == '{season}'")
 
-    # owrite new csv after cut
+	# query the data by season and tournament
+	#cut_df = df.query(f"seasonName == '{season}' & tournamentName == 'Premier League' & teamRegionName == 'England'")
+
+    # write new .csv after cut, sorted by team
+	cut_df = cut_df.sort_values(by=["teamName"])
 	cut_df.to_csv("dataset/" + new_file)
+
 	return
 
 
