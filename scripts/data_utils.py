4c30dee464528a09f4a9859084445e26abcd273b
zascerta <giammarco.daless@gmail.com>
2022-03-07 12:51:15 +0100

    all season query in one shot

--- scripts/data_utils.py
+++ scripts/data_utils.py
@@ -1,5 +1,5 @@
+import os
 import pandas as pd
-import csv
 
 DATA_DIR   = "dataset/fbref/"
 FBREF_DEF  = "dataset/fbref/defense.csv"
@@ -7,23 +7,32 @@ FBREF_GCA  = "dataset/fbref/gca.csv"
 FBREF_INFO = "dataset/fbref/info.csv"
 
 
-def dataCutSeason(season: str, in_file: str=DATA_DIR, out_file: str="export_std_19-20.csv"):
-	in_file  = in_file + "standard.csv" 
-	out_file = DATA_DIR + out_file
+def fbrefGetSeason(season: str, in_dir: str=DATA_DIR, out_file: str="export.csv"):
+	"""
+	Extract only rows relative to season `season` from the tables in `in_dir`.
+	"""
+	for table in os.listdir(in_dir):
+		if table[:6] == "export" or table[-4:] == ".zip" or table == "info.csv":
+			continue
 
-	# read file data
-	df = pd.read_csv(in_file, delimiter=',')
+		else:
+			print("...querying on", table)	
+			in_file  = in_dir + table 
+			out_file = in_dir + "export_19-20_" + table
 
-	# query the data by season and tournament
-	cut_df = df.query(f"season == '{season}'")
+			# read file data
+			df = pd.read_csv(in_file, delimiter=',')
 
-    # write new .csv after cut, sorted by team
-	cut_df = cut_df.sort_values(by=["id"])
-	cut_df.to_csv(out_file)
+			# query the data by season 
+			cut_df = df.query(f"season == '{season}'")
 
+			# write new .csv after cut, sorted by id
+			cut_df = cut_df.sort_values(by=["id"])
+			cut_df.to_csv(out_file)
+			
 	return
 
 
 
 if __name__ == "__main__":
-	dataCutSeason(season="2019-2020")
+	fbrefGetSeason(season="2019-2020")
