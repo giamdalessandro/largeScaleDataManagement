2173fbdee5fa983090f2f8fc1ec5f6632ccc3d90
zascerta <giammarco.daless@gmail.com>
2022-03-07 12:58:49 +0100

    aa

--- scripts/data_utils.py
+++ scripts/data_utils.py
@@ -1,13 +1,11 @@
 import os
 import pandas as pd
 
-DATA_DIR   = "dataset/fbref/"
-FBREF_DEF  = "dataset/fbref/defense.csv"
-FBREF_GCA  = "dataset/fbref/gca.csv"
-FBREF_INFO = "dataset/fbref/info.csv"
+FBREF_DIR = "dataset/fbref/"
+FM_DATA   = "dataset/whoscored/fm20-columns.csv"
 
 
-def fbrefGetSeason(season: str, in_dir: str=DATA_DIR, out_file: str="export.csv"):
+def fbrefGetSeason(season: str, in_dir: str=FBREF_DIR, out_file: str="export.csv"):
 	"""
 	Extract only rows relative to season `season` from the tables in `in_dir`.
 	"""
@@ -32,6 +30,17 @@ def fbrefGetSeason(season: str, in_dir: str=DATA_DIR, out_file: str="export.csv"
 			
 	return
 
+def fmGetSeason(season: str, in_file: str=FM_DATA, out_file: str="export.csv"):
+	# read file data
+	df = pd.read_csv(in_file, delimiter=',')
+
+	# query the data by season 
+	cut_df = df.query(f"season == '{season}'")
+
+	# write new .csv after cut, sorted by id
+	cut_df = cut_df.sort_values(by=["id"])
+	cut_df.to_csv(out_file)
+	return
 
 
 if __name__ == "__main__":
