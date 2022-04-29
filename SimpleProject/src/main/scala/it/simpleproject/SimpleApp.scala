1f78e7e2100c7dfff8f6207165ba879c3d810ffc
zascerta <giammarco.daless@gmail.com>
2022-04-29 16:48:48 +0200

    metals fixes

--- SimpleProject/src/main/scala/it/simpleproject/SimpleApp.scala
+++ SimpleProject/src/main/scala/it/simpleproject/SimpleApp.scala
@@ -12,7 +12,7 @@ object SimpleApp {
     val logData = spark.read.textFile(logFile).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
-    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    println(s"Lines with a: $numAs, Lines with b: $numBs\n\n")
     spark.stop()
   }
 }
